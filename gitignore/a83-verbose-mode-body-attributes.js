4bd7864aa07db1c3d1dd30231761aa844bb8e033
aeon022 <gerwin.weiher@gmail.com>
2026-02-20 17:47:48 +0100

    gitignore Ordner entfernt

--- gitignore/a83-verbose-mode-body-attributes.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.dataset.verboseDefault = 'on';   // oder 'off'
-  document.body.dataset.verboseScope = 'page';   // 'page' oder 'site'
-});
